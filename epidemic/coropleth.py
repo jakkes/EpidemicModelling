df0fda997647f01f47040e4e41d9a471e89496bf
thaggus <thogust@kth.se>
2020-04-05 18:28:15 +0200

    updates

--- epidemic/coropleth.py
+++ epidemic/coropleth.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import geopandas
 
-sweden = geopandas.read_file("./data/sweden-regional/geo/Lan_Sweref99TM_region.shp")
 
-sweden.plot()
-
-plt.show()
+if __name__ == "__main__":
+    sweden = geopandas.read_file("./data/sweden-regional/geo/Lan_Sweref99TM_region.shp")
+    sweden.plot()
+    plt.show()
